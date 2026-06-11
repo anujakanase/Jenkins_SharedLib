95313a9aff8b0459a4a4f65c910faa1c863c2db6
Anuja Kanase <anujakanase99@gmail.com>
2026-06-11 16:28:19 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,5 +1,17 @@
 def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-  }
+
+    withSonarQubeEnv("${SonarQubeAPI}") {
+
+        withCredentials([
+            string(credentialsId: 'sonar-token', variable: 'SONAR_TOKEN')
+        ]) {
+
+            sh """
+            \$SONAR_HOME/bin/sonar-scanner \
+            -Dsonar.projectName=${Projectname} \
+            -Dsonar.projectKey=${ProjectKey} \
+            -Dsonar.login=\$SONAR_TOKEN
+            """
+        }
+    }
 }
